71351e05e342332af1064dcaf3706e0a91a9b91a
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-03-06 20:15:07 +0100

    Practiice

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Counter from "./Counter";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Compound Component Pattern</h1>
+      {/* <Counter
+        iconIncrease="+"
+        iconDecrease="-"
+        label="My NOT so flexible counter"
+        hideLabel={false}
+        hideIncrease={false}
+        hideDecrease={false}
+      /> */}
+
+      <Counter>
+        <Counter.Decrease icon="-" />
+        <Counter.Count />
+        <Counter.Increase icon="+" />
+        <Counter.Label>My super so flexible counter</Counter.Label>
+      </Counter>
+      <div>
+        <Counter>
+          <Counter.Decrease icon="-" />
+          <Counter.Count />
+          <Counter.Increase icon="+" />
+        </Counter>
+      </div>
+    </div>
+  );
+}
